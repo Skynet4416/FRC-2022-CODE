4f4523b4f01816cdb82426512614f4679b1b3e39
Nir <56742172+yotam201410@users.noreply.github.com>
2022-01-18 19:26:11 +0200

    set talonFX

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+// import java.util.Set;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.InvertType;
+// import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+// import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+// import com.ctre.phoenix.motorcontrol.TalonFXSensorCollection;
+// import com.ctre.phoenix.motorcontrol.TalonFXSimCollection;
+import frc.robot.Constants.Shooter.Motors;
+
+public class ShooterSubsystem extends PIDSubsystem {
+    private TalonFX _master = new TalonFX(Motors.Kmaster); // not sure we will use this engine
+    private TalonFX _slave = new TalonFX(Motors.Kslave); // not sure we will use this engine
+    private PIDController PIDcontroller = new PIDController(0, 0, 0);
+
+    public ShooterSubsystem() {
+        super(new PIDController(0, 0, 0));
+        _slave.configFactoryDefault();
+        _master.configFactoryDefault();
+        _slave.follow(_master);
+        _slave.setInverted(InvertType.InvertMotorOutput);
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    protected void useOutput(double output, double setpoint) {
+        // TODO Auto-generated method stub
+        _master.set(ControlMode.PercentOutput, output);
+        _slave.set(ControlMode.PercentOutput, output); // might not work (TEST) check wtice
+
+    }
+
+    @Override
+    protected double getMeasurement() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
