6ea874cdf53c500c790ec4c75ae03e0fc34d62f1
Nir <56742172+yotam201410@users.noreply.github.com>
2022-01-18 19:56:21 +0200

    forgot this file oops

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,5 +19,12 @@ public final class Constants {
             public static final int  Kmaster = -1;
             public static final int  Kslave = -1;
         }
+        public final class PID
+        {
+            //needs to be set to real values
+            public static final double kP = 0;
+            public static final double kI = 0;
+            public static final double kD = 0;
+        }
     }
 }
