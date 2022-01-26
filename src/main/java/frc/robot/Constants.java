633c8f254bac680e0b3036a91dedf799affb24e3
R C N <plbwymw@gmail.com>
2022-01-26 16:44:05 +0200

    formatted code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,33 +5,39 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public static class Shooter{
-        public static class Physics{
+    public static class Shooter {
+        public static class Physics {
             public static final int MAX_RPM = 6000;
-            public static double diamater = 101.6; //in mm
-            public static double gravitational_acceleration_near_earth = 9.83; //in m/s^2
-            public static double ball_mass =  0.267619498; //in kg 
-            public static double density_of_air = 1.293;//in kg/m^3
+            public static double diamater = 101.6; // in mm
+            public static double gravitational_acceleration_near_earth = 9.83; // in m/s^2
+            public static double ball_mass = 0.267619498; // in kg
+            public static double density_of_air = 1.293;// in kg/m^3
             public static double drag_coefficient = 0.47; // no units
             public static double RPM_presentange_loss = 0.0;
-            public static double circumference = (diamater/1000) * Math.PI;
-            public static double cross_area = Math.PI * Math.pow((diamater/2)/1000,2); //m^2
-            public static double drag_thing = (density_of_air * drag_coefficient * cross_area)/2; // kg/m
-            public static double resolution = 0.1;//in s 
+            public static double circumference = (diamater / 1000) * Math.PI;
+            public static double cross_area = Math.PI * Math.pow((diamater / 2) / 1000, 2); // m^2
+            public static double drag_thing = (density_of_air * drag_coefficient * cross_area) / 2; // kg/m
+            public static double resolution = 0.1;// in s
             public static double shooter_height = 0; // in meter
-            public static double threashold_x = 0;//in meter
-            public static double threashold_y = 0;//in meter
-          
-    public class Intake{
-        public class Motors{
+            public static double threashold_x = 0;// in meter
+            public static double threashold_y = 0;// in meter
+        }
+    }
+
+    public class Intake {
+        public class Motors {
             public static final int kIntake = -1;
         }
     }
--- src/main/java/frc/robot/commands/IntakeSpinUp.java
+++ src/main/java/frc/robot/commands/IntakeSpinUp.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class IntakeSpinUp extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
   private final IntakeSubsystem m_subsystem;
 
   /**
@@ -27,12 +27,13 @@ public class IntakeSpinUp extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-      m_subsystem.setIntake(Constants.Intake.Motors.PowerPrecentage);
+    m_subsystem.setIntake(Constants.Intake.Motors.PowerPrecentage);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
 
   // Called once the command ends or is interrupted.
   @Override
