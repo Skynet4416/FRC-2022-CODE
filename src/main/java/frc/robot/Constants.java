1fbffccf01c3063dda379097c852e5852fd003c7
yotam <yotam201410@gmail.com>
2022-01-26 15:03:28 +0200

    added the elevator subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,12 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public class Elevator{
+        public class Motors{
+            public static final int Kmaster = -1;
+            public static final int Kslave = -1;
+            public static final double speed = 1;
+        }
+    }
+}
