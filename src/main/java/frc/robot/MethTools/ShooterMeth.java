3a8472148d68226ca2f78cad0517111e196a6533
Nir <92321495+Aldema1@users.noreply.github.com>
2022-02-19 12:52:34 +0200

    finished updating the physics code for shooter

--- src/main/java/frc/robot/MethTools/ShooterMeth.java
+++ src/main/java/frc/robot/MethTools/ShooterMeth.java
@@ -108,13 +108,14 @@ public class ShooterMeth {
     {
         Point lastPoint = new Point();
         Point currePoint = new Point();
+        boolean prev_fit = false;
         for(int i = 0; i<trajectory[0].length; i++)
         {
             lastPoint.x = currePoint.x;
             lastPoint.y = currePoint.y;
             currePoint.x = trajectory[i][0];
             currePoint.y = trajectory[i][1];
-            if (currePoint.x > Physics.hub_distance + Physics.hub_diameter /2)
+            if (currePoint.x > Physics.hub_distance + Physics.hub_diameter /2 && current_point.y>Physics.hub_height)
                 return false;
             else if (currePoint.x > Physics.hub_height + Physics.threashold + Physics.diamater && lastPoint.y > (Physics.hub_height + Physics.threashold + Physics.diamater) && currePoint.x > (Physics.hub_distance - Physics.hub_diameter/2) && currePoint.x < Physics.hub_distance + Physics.hub_diameter && lastPoint.x > Physics.hub_distance-Physics.hub_diameter/2)
             {
@@ -128,16 +129,28 @@ public class ShooterMeth {
     {
         boolean done = false;
         double rpm = Physics.MAX_RPM *(1-Physics.RPM_presentange_loss);
-        double best_rpm = -1;
+        double best_rpm = Physics.MAX_RPM*(1-Physics.RPM_presentange_loss);
         double best_angle = -1;
         double[][] line;
         for(int angle = 45; angle<90; angle++)
         {
+            rpm = best_rpm;
             line = ReturnArrayOfPos(rpm, Math.toRadians(90-angle+45));
-            while (fits_in_hub(line))
+            boolean prevfit = false
+            while (rpm - Physics.optimisation_RPM_Resolution > 0)
             {
-                best_rpm = rpm;
-                best_angle = (90-angle) + 45;
+                if( fits_in_hub(line))
+                {
+                    prev_fit = true;
+                    best_rpm = rpm;
+                    best_angle = (90-angle + 45)
+                }
+                else{
+                    if(prev_fit)
+                    {
+                        rpm = 0;
+                    }
+                }
                 rpm -= Physics.optimisation_RPM_Resolution;
                 line = ReturnArrayOfPos(rpm, Math.toRadians(90-angle+45));
             }
